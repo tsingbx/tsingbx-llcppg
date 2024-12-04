0aefc1147439ad7ac98f4d57258034d36e88db34
luoliwoshang <2643523683@qq.com>
2024-12-04 17:46:45 +0800

    gogensig:type conv wrap error

--- cmd/gogensig/convert/type.go
+++ cmd/gogensig/convert/type.go
@@ -4,6 +4,7 @@ This file is used to convert type from ast type to types.Type
 package convert
 
 import (
+	"errors"
 	"fmt"
 	"go/token"
 	"go/types"
@@ -15,6 +16,10 @@ import (
 	"github.com/goplus/llcppg/cmd/gogensig/config"
 	"github.com/goplus/llcppg/cmd/gogensig/convert/names"
 	"github.com/goplus/llcppg/cmd/gogensig/convert/sizes"
+)
+
+var (
+	ErrTypeConv = errors.New("error convert type")
 )
 
 type HeaderInfo struct {
@@ -76,7 +81,7 @@ func (p *TypeConv) ToType(expr ast.Expr) (types.Type, error) {
 	case *ast.RecordType:
 		return p.RecordTypeToStruct(t)
 	default:
-		return nil, fmt.Errorf("unsupported type: %T", expr)
+		return nil, fmt.Errorf("%w: unsupported type %T", ErrTypeConv, expr)
 	}
 }
 
