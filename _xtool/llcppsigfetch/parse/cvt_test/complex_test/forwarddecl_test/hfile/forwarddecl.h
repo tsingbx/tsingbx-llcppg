a4ff19c247320988d8d66b463f0fe8fefa3f4944
luoliwoshang <2643523683@qq.com>
2024-12-12 17:44:53 +0800

    llcppsigfetch:patch unexpect typedef from forwarddecl impl test

--- _xtool/llcppsigfetch/parse/cvt_test/complex_test/forwarddecl_test/hfile/forwarddecl.h
+++ _xtool/llcppsigfetch/parse/cvt_test/complex_test/forwarddecl_test/hfile/forwarddecl.h
@@ -1,3 +1,13 @@
+struct Foo
+{
+    struct bar *b;
+};
+typedef struct bar bar;
+struct bar
+{
+    int a;
+};
+
 typedef struct sqlite3_pcache_page sqlite3_pcache_page;
 struct sqlite3_pcache_page
 {
@@ -12,6 +22,18 @@ struct sqlite3_pcache_methods2
     int iVersion;
     void (*xShutdown)(void *);
     sqlite3_pcache *(*xCreate)(int szPage, int szExtra, int bPurgeable);
+};
+
+typedef struct sqlite3_file sqlite3_file;
+struct sqlite3_file
+{
+    const struct sqlite3_io_methods *pMethods; /* Methods for an open file */
+};
+
+typedef struct sqlite3_io_methods sqlite3_io_methods;
+struct sqlite3_io_methods
+{
+    int (*xUnfetch)(sqlite3_file *, int iOfst, void *p);
 };
 
 #define LUA_IDSIZE 60
