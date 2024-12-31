5a10abad9342270bfb5959d645d8a71e7bf95317
luoliwoshang <2643523683@qq.com>
2024-12-31 18:55:51 +0800

    demo test:isl intersect

---
+++ _llcppgtest/isl/demo/intersect/intersect.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"isl"
+
+	"github.com/goplus/llgo/c"
+)
+
+func main() {
+	ctx := isl.Alloc()
+	set1 := ctx.ReadFromStr(c.Str("{ [x] : 0 <= x <= 10 }"))
+	set2 := ctx.ReadFromStr(c.Str("{ [x] : 5 <= x <= 15 }"))
+
+	intersec := set1.Copy().Intersect(set2.Copy())
+
+	set1.Dump()
+	set2.Dump()
+	intersec.Dump()
+
+	set1.Free()
+	set2.Free()
+	intersec.Free()
+	ctx.Free()
+}
