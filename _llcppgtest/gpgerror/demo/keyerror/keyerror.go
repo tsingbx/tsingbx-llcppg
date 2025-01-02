493343cf7257b7b696be5694dda2e33172c9088b
luoliwoshang <2643523683@qq.com>
2025-01-02 18:11:07 +0800

    gogensig:remove enum item prefix

--- _llcppgtest/gpgerror/demo/keyerror/keyerror.go
+++ _llcppgtest/gpgerror/demo/keyerror/keyerror.go
@@ -6,21 +6,21 @@ import (
 	"github.com/goplus/llgo/c"
 )
 
-var GPGERRSOURCEMASK = gpgerror.SourceTGPGERRSOURCEDIM - 1
-var GPGERRCODEMASK = gpgerror.CodeTGPGERRCODEDIM - 1
-var GPGERRSOURCESHIFT = 24
+var SOURCEMASK = gpgerror.SourceTSOURCEDIM - 1
+var CODEMASK = gpgerror.CodeTCODEDIM - 1
+var SOURCESHIFT = 24
 
 func ErrMake(source gpgerror.SourceT, code gpgerror.CodeT) gpgerror.ErrorT {
-	if code == gpgerror.CodeTGPGERRNOERROR {
-		return gpgerror.ErrorT(gpgerror.CodeTGPGERRNOERROR)
+	if code == gpgerror.CodeTNOERROR {
+		return gpgerror.ErrorT(gpgerror.CodeTNOERROR)
 	}
 
-	return gpgerror.ErrorT(((c.Int(source) & c.Int(GPGERRSOURCEMASK)) << c.Int(GPGERRSOURCESHIFT)) | (c.Int(code) & c.Int(GPGERRCODEMASK)))
+	return gpgerror.ErrorT(((c.Int(source) & c.Int(SOURCEMASK)) << c.Int(SOURCESHIFT)) | (c.Int(code) & c.Int(CODEMASK)))
 }
 
 func main() {
 	gpgerror.Init()
-	err := ErrMake(gpgerror.SourceTGPGERRSOURCEUSER1, gpgerror.CodeTGPGERRBADKEY)
+	err := ErrMake(gpgerror.SourceTSOURCEUSER1, gpgerror.CodeTBADKEY)
 	errStr := err.Strerror()
 	source := err.Strsource()
 	c.Printf(c.Str("Error: %s\n"), errStr)
--- cmd/gogensig/convert/_testdata/enum/hfile/temp.h
+++ cmd/gogensig/convert/_testdata/enum/hfile/temp.h
@@ -18,3 +18,26 @@ typedef enum {
 } algorithm_t2;
 
 typedef algorithm_t algorithm;
+
+typedef enum {
+    GPG_ERR_NO_ERROR = 0,
+    GPG_ERR_GENERAL = 1,
+    GPG_ERR_UNKNOWN_PACKET = 2,
+    GPG_ERR_UNKNOWN_VERSION = 3,
+    GPG_ERR_PUBKEY_ALGO = 4,
+    GPG_ERR_DIGEST_ALGO = 5,
+    GPG_ERR_BAD_PUBKEY = 6,
+    GPG_ERR_BAD_SECKEY = 7,
+    GPG_ERR_BAD_SIGNATURE = 8,
+    GPG_ERR_NO_PUBKEY = 9,
+    GPG_ERR_CHECKSUM = 10,
+    GPG_ERR_BAD_PASSPHRASE = 11,
+    GPG_ERR_CIPHER_ALGO = 12,
+    GPG_ERR_KEYRING_OPEN = 13,
+    GPG_ERR_INV_PACKET = 14,
+    GPG_ERR_INV_ARMOR = 15,
+    GPG_ERR_NO_USER_ID = 16,
+    GPG_ERR_NO_SECKEY = 17,
+    GPG_ERR_WRONG_SECKEY = 18,
+    GPG_ERR_BAD_KEY = 19,
+} gpg_err_code_t;
