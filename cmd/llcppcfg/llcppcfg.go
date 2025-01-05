500e81e00dcd0a551ac6660c6197c9982a8776fe
tsingbx <tsingbx@gmail.com>
2025-01-05 17:43:18 +0800

    llcppcfg: support ignore internal subdir

--- cmd/llcppcfg/llcppcfg.go
+++ cmd/llcppcfg/llcppcfg.go
@@ -11,7 +11,7 @@ import (
 
 func printHelp() {
 	log.Println(`llcppcfg is to generate llcppg.cfg file.
-usage: llcppcfg [-cpp|-help|-expand|-sort] libname`)
+usage: llcppcfg [-cpp|-help|-expand|-sort|-excludes] libname`)
 	flag.PrintDefaults()
 }
 
@@ -22,7 +22,9 @@ func main() {
 	flag.BoolVar(&expand, "expand", false, "expand pkg-config command to result")
 	flag.BoolVar(&sortByDep, "sort", true, "expand every cflag and list it's include files and sort include files by dependency")
 	extsString := ""
-	flag.StringVar(&extsString, "exts", ".h", "extra include file extensions for example \"-exts=.h .hpp .hh\"")
+	flag.StringVar(&extsString, "exts", ".h", "extra include file extensions for example -exts=\".h .hpp .hh\"")
+	excludes := ""
+	flag.StringVar(&excludes, "excludes", "internal", "exclude all header files in subdir of include expamle -excludes=\"internal impl\"")
 	flag.Usage = printHelp
 	flag.Parse()
 	if help || len(os.Args) <= 1 {
@@ -43,7 +45,8 @@ func main() {
 		return llcppgcfg.NormalMode
 	}
 	exts := strings.Fields(extsString)
-	buf, err := llcppgcfg.GenCfg(name, cpp, fnToCfgExpandMode(), exts)
+	excludeSubdirs := strings.Fields(excludes)
+	buf, err := llcppgcfg.GenCfg(name, cpp, fnToCfgExpandMode(), exts, excludeSubdirs)
 	if err != nil {
 		log.Fatal(err)
 	}
