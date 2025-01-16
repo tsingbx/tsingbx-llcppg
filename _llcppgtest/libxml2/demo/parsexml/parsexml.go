9d8c332031751ea821cbba496a8f5679981f37c8
luoliwoshang <2643523683@qq.com>
2025-01-16 14:01:07 +0800

    demo test:libxml2

---
+++ _llcppgtest/libxml2/demo/parsexml/parsexml.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"libxml2"
+	"unsafe"
+
+	"github.com/goplus/llgo/c"
+)
+
+func main() {
+	libxml2.XmlInitParser()
+	xml := "<?xml version='1.0'?><root><person><name>Alice</name><age>25</age></person></root>"
+	doc := libxml2.XmlReadMemory((*int8)(unsafe.Pointer(unsafe.StringData(xml))), c.Int(len(xml)), nil, nil, 0)
+	if doc == nil {
+		panic("Failed to parse XML")
+	}
+	docPtr := (*libxml2.XmlDoc)(unsafe.Pointer(doc))
+	root := docPtr.XmlDocGetRootElement()
+	c.Printf(c.Str("Root element: %s\n"), root.Name)
+	libxml2.XmlFreeDoc(doc)
+	libxml2.XmlCleanupParser()
+}
