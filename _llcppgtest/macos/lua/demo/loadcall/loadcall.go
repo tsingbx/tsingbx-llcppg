6032eba3f2b10a907712104b9666df28fb8ae426
luoliwoshang <2643523683@qq.com>
2025-02-20 10:28:26 +0800

    llcppg:demotest with mix in linux

---
+++ _llcppgtest/macos/lua/demo/loadcall/loadcall.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	_ "unsafe"
+
+	"lua"
+
+	"github.com/goplus/llgo/c"
+)
+
+func main() {
+	L := lua.Newstate__1()
+	defer L.Close()
+
+	L.Openlibs()
+	if res := L.Loadstring(c.Str("print('hello world')")); res != lua.OK {
+		println("error")
+	}
+	if res := L.Pcallk(0, 0, 0, 0, nil); res != lua.OK {
+		println("error")
+	}
+
+}
+
+/* Expected output:
+hello world
+*/
