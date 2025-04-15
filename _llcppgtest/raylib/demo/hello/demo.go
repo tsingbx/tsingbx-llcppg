eef7bf0226d90efeff276d03931c4b5ed28d0a02
张之阳 <51194195+luoliwoshang@users.noreply.github.com>
2025-04-15 21:11:08 +0800

    demo test:raylib test demo (#257)
    * demo test:raylib test demo
    
    * demo test:set no render function in ci raylib test

--- _llcppgtest/raylib/demo/hello/demo.go
+++ _llcppgtest/raylib/demo/hello/demo.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"raylib"
 
 	"github.com/goplus/llgo/c"
@@ -11,9 +13,22 @@ func main() {
 	const screenHeight = 450
 	blue := raylib.GetColor(0xFF00F179)
 	white := raylib.GetColor(0xFFFFFFFF)
+
+	// run in headless mode(CI) without proper graphics hardware or drivers,
+	// causing OpenGL initialization to fail.
+	// so we check these function which is not related to graphics initialization.
+	if os.Getenv("CI") == "true" {
+		raylib.SetRandomSeed(42)
+		randomValue := raylib.GetRandomValue(1, 100)
+		fmt.Println("Random value:", randomValue)
+		color := raylib.GetColor(0xFF00F179)
+		fmt.Println("Color components:", color.R, color.G, color.B, color.A)
+		return
+	}
+
 	raylib.InitWindow(screenWidth, screenHeight, c.Str("Raylib DEMO"))
 	startTime := raylib.GetTime()
-	for raylib.WindowShouldClose() == 0 {
+	for !raylib.WindowShouldClose() {
 		currentTime := raylib.GetTime()
 		if currentTime-startTime >= 1.0 {
 			break
