70bb55f6696f2648b47ae2f06790cc059c5191c3
张之阳 <51194195+luoliwoshang@users.noreply.github.com>
2025-04-24 12:06:46 +0800

    gogensig/chore:mv unuse _comment_test to converter test (#268)

--- cmd/gogensig/convert/_testdata/keepcomment/hfile/temp.h
+++ cmd/gogensig/convert/_testdata/keepcomment/hfile/temp.h
@@ -1,9 +1,36 @@
-/// Foo comment
-struct Foo {
+/**
+Foo comment
+*/
+struct Foo1 { int a; double b; bool c; };
+/*
+Foo comment
+*/
+struct Foo2 {
     int a;
     double b;
     bool c;
 };
-
+/// Foo comment
+struct Foo3 {
+    int a;
+    double b;
+    bool c;
+};
+// Foo comment
+struct Foo4 {
+    int a;
+    double b;
+    bool c;
+};
+/**
+ExecuteFoo comment
+*/
+int ExecuteFoo1(int a, Foo1 b);
+/*
+ExecuteFoo comment
+*/
+int ExecuteFoo2(int a, Foo2 b);
 /// ExecuteFoo comment
-int ExecuteFoo(int a, Foo b);
+int ExecuteFoo3(int a, Foo3 b);
+// ExecuteFoo comment
+int ExecuteFoo4(int a, Foo4 b);
