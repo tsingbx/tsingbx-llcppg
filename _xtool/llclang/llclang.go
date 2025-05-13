992f1b37e64578e68e628d4f2b1d91232a055b69
xushiwei <x@goplus.org>
2025-05-13 19:12:40 +0800

    Update llclang.go

--- _xtool/llclang/llclang.go
+++ _xtool/llclang/llclang.go
@@ -2,7 +2,8 @@ package main
 
 /*
 1) input: llclang parseIntermediateFile <filename> <mode>
-* output: AST in JSON format
+* output: AST in JSON format to stdout
+* error: non-zero exit code, and error message to stderr
 
 2) input: XXX
 * output: XXX
