4071e392df98fe9e9fe318e6e9cee55630a1ca14
luoliwoshang <2643523683@qq.com>
2025-05-14 10:43:42 +0800

    llclang/parser:libclang util from llcppsymg/tool/clang

---
+++ _xtool/llclang/internal/parser/clang.go
@@ -0,0 +1,115 @@
+package parser
+
+import (
+	"errors"
+	"unsafe"
+
+	"github.com/goplus/lib/c"
+	"github.com/goplus/lib/c/clang"
+)
+
+type LibClangConfig struct {
+	File  string
+	Temp  bool
+	Args  []string
+	IsCpp bool
+	Index *clang.Index
+}
+
+type Visitor func(cursor, parent clang.Cursor) clang.ChildVisitResult
+
+type InclusionVisitor func(included_file clang.File, inclusions []clang.SourceLocation)
+
+const TEMP_FILE = "temp.h"
+
+func CreateTranslationUnit(config *LibClangConfig) (*clang.Index, *clang.TranslationUnit, error) {
+	// default use the c/c++ standard of clang; c:gnu17 c++:gnu++17
+	// https://clang.llvm.org/docs/CommandGuide/clang.html
+	allArgs := append(defaultArgs(config.IsCpp), config.Args...)
+
+	cArgs := make([]*c.Char, len(allArgs))
+	for i, arg := range allArgs {
+		cArgs[i] = c.AllocaCStr(arg)
+	}
+
+	var index *clang.Index
+	if config.Index != nil {
+		index = config.Index
+	} else {
+		index = clang.CreateIndex(0, 0)
+	}
+
+	var unit *clang.TranslationUnit
+
+	if config.Temp {
+		content := c.AllocaCStr(config.File)
+		tempFile := &clang.UnsavedFile{
+			Filename: c.Str(TEMP_FILE),
+			Contents: content,
+			Length:   c.Ulong(c.Strlen(content)),
+		}
+
+		unit = index.ParseTranslationUnit(
+			tempFile.Filename,
+			unsafe.SliceData(cArgs), c.Int(len(cArgs)),
+			tempFile, 1,
+			clang.DetailedPreprocessingRecord,
+		)
+
+	} else {
+		cFile := c.AllocaCStr(config.File)
+		unit = index.ParseTranslationUnit(
+			cFile,
+			unsafe.SliceData(cArgs), c.Int(len(cArgs)),
+			nil, 0,
+			clang.DetailedPreprocessingRecord,
+		)
+	}
+
+	if unit == nil {
+		return nil, nil, errors.New("failed to parse translation unit")
+	}
+
+	return index, unit, nil
+}
+
+func GetLocation(loc clang.SourceLocation) (file clang.File, line c.Uint, column c.Uint, offset c.Uint) {
+	loc.SpellingLocation(&file, &line, &column, &offset)
+	return
+}
+
+// Traverse up the semantic parents
+func BuildScopingParts(cursor clang.Cursor) []string {
+	var parts []string
+	for cursor.IsNull() != 1 && cursor.Kind != clang.CursorTranslationUnit {
+		name := cursor.String()
+		qualified := c.GoString(name.CStr())
+		parts = append([]string{qualified}, parts...)
+		cursor = cursor.SemanticParent()
+		name.Dispose()
+	}
+	return parts
+}
+
+func VisitChildren(cursor clang.Cursor, fn Visitor) c.Uint {
+	return clang.VisitChildren(cursor, func(cursor, parent clang.Cursor, clientData unsafe.Pointer) clang.ChildVisitResult {
+		cfn := *(*Visitor)(clientData)
+		return cfn(cursor, parent)
+	}, unsafe.Pointer(&fn))
+}
+
+func GetInclusions(unit *clang.TranslationUnit, visitor InclusionVisitor) {
+	clang.GetInclusions(unit, func(inced clang.File, incin *clang.SourceLocation, incilen c.Uint, data c.Pointer) {
+		ics := unsafe.Slice(incin, incilen)
+		cfn := *(*InclusionVisitor)(data)
+		cfn(inced, ics)
+	}, unsafe.Pointer(&visitor))
+}
+
+func defaultArgs(isCpp bool) []string {
+	args := []string{"-x", "c"}
+	if isCpp {
+		args = []string{"-x", "c++"}
+	}
+	return args
+}
